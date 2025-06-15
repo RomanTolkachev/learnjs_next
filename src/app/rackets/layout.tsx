4ccefc7798c9f54622c2b951fe592dd461bbfb2b
Cyrtolat <cyrtolat@gmail.com>
2025-06-16 00:44:20 +0300

    fixed

--- src/app/rackets/layout.tsx
+++ src/app/rackets/layout.tsx
@@ -1,53 +1,16 @@
-"use client"
-
-import { IBrand } from '@/entities';
-import { SidebarSelect } from '@/shared/ui/sidebar_select';
-import { usePathname, useRouter } from 'next/navigation';
 import React, { FunctionComponent, ReactNode } from 'react';
 
 interface Props {
-    className?: string;
     children: ReactNode
     modal: ReactNode
 }
 
-type TBrands = IBrand | "All";
-
-// тут, наверно, лучше объект т.к ключ это label для Select, а значение это само значение и они могут отличаться (теоретически)
-const routerValues: Record<TBrands, TBrands> = {
-    All: "All",
-    Babolat: "Babolat",
-    Head: "Head",
-    Wilson: "Wilson",
-    Yonex: "Yonex"
-}
-
-const layout: FunctionComponent<Props> = ({ className, children, modal }) => {
-
-    const pathname = usePathname();
-    const router = useRouter();
-
-    function navigate(to: string): void {
-        router.push(to);
-    }
-
-    const regex = /\/rackets(?:\/([^\/]+))?$/;
-
+const layout: FunctionComponent<Props> = ({ children, modal }) => {
     return (
-        <section className={`${className} mx-auto px-10 w-full grid grid-cols-[auto_auto]`}>
-            <aside>
-                <h3 className='font-semibold text-xs text-muted-foreground/60 mb-1'>Бренд</h3>
-                <SidebarSelect
-                    values={routerValues}
-                    currentValue={pathname.match(regex)?.[1] ?? "All"}
-                    setter={to => navigate(to === "All" ? "/rackets" : `/rackets/${to as TBrands}`)}
-                />
-            </aside>
-            <div>
-                {children}
-                {modal}
-            </div>
-        </section>
+        <>
+            {modal}
+            {children}
+        </>
     );
 };
 
--- src/app/rackets/page.tsx
+++ src/app/rackets/page.tsx
@@ -1,19 +1,54 @@
+"use client"
+
+import { IBrand } from '@/entities';
 import { rackets } from '@/lib/mock';
 import { ProductList } from '@/shared/ui/product_list';
+import { SidebarSelect } from '@/shared/ui/sidebar_select';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import React, { FunctionComponent } from 'react';
 
 interface Props {
     className?: string;
-    params: Promise<{ brand: string }>
 }
 
-const Page: FunctionComponent<Props> = async ({ params }) => {
+type TBrands = IBrand | "All";
 
-    const { brand } = await params;
+// тут, наверно, лучше объект т.к ключ это label для Select, а значение это само значение и они могут отличаться (теоретически)
+const routerValues: Record<TBrands, TBrands> = {
+    All: "All",
+    Babolat: "Babolat",
+    Head: "Head",
+    Wilson: "Wilson",
+    Yonex: "Yonex"
+}
+
+const Page: FunctionComponent<Props> = ({ className }) => {
+
+    const pathname = usePathname();
+    const router = useRouter();
+    const { brand } = useParams();
 
     const filteredRackets = brand ? rackets.filter(racket => racket.brand.name === brand) : rackets;
 
-    return <ProductList data={filteredRackets} />;
+    function navigate(to: string): void {
+        router.push(to);
+    }
+
+    const regex = /\/rackets(?:\/([^\/]+))?$/;
+
+    return (
+        <section className={`${className} mx-auto px-10 w-full grid grid-cols-[auto_auto]`}>
+            <aside>
+                <h3 className='font-semibold text-xs text-muted-foreground/60 mb-1'>Бренд</h3>
+                <SidebarSelect
+                    values={routerValues}
+                    currentValue={pathname.match(regex)?.[1] ?? "All"}
+                    setter={to => navigate(to === "All" ? "/rackets" : `/rackets/${to as TBrands}`)}
+                />
+            </aside>
+            <ProductList data={filteredRackets} />;
+        </section>
+    )
 
 };
 
